ea8410f6a0d6babae1f28d496affd88ac4a98877
马到成功 <ctao8392@gmail.com>
2026-03-05 20:28:48 +0800

    测试项目文件

---
+++ hello.py
@@ -0,0 +1,2 @@
+print("Hello, World!")
+print("我的名字是[你的名字]，今天开始学Python！")
---
+++ n.py
@@ -0,0 +1,48 @@
+x=5
+if x > 4: print("Greater than 4")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+his = input("Enter Hours:")
+her = input("Enter Rate:")
+Pay = float(his)*float(her)
+print("Pay:",Pay)      
